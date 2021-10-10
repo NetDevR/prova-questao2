54027da096ecf05ddc4bda148134376ff8698b19
Ednaldo Neto <ednaldonetoaraujo18@gmail.com>
2021-10-10 19:57:59 -0300

    Criação do empréstimo e seu array

--- src/ArrayEmprestimos.java
+++ src/ArrayEmprestimos.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class ArrayEmprestimos {
+    private static ArrayList<Emprestimos> listaEmprestimos = new ArrayList<>();
+
+    public static ArrayList<Emprestimos> getListaEmprestimos(){
+        return listaEmprestimos;
+    }
+
+    static public void cadastrar(Emprestimos e){
+        listaEmprestimos.add(e);
+    }
 }
